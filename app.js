d17f3c269c5c238b7fe718c6cf418da08b49a84d
vehas <sky.48478@gmail.com>
2015-11-07 15:17:32 +0700

    add morgan for logging

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 'use strict';
 let express = require('express'),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  morgan = require('morgan');
 let config = require('./config.json');
 let app = express(),
   port = config.port;
-
+// set up
+app.use(morgan(':method :url'));
 app.engine('jade', require('jade').__express);
 app.set('view engine', 'jade');
 app.set('views', './views');
