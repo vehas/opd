01aadaf11705aa5b991dfdaf39cbc3b295ab146e
vehas <sky.48478@gmail.com>
2015-11-21 17:41:18 +0700

    resolve  doctor detail

---
+++ dist/js/nurse.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('#myModal').on('hidden.bs.modal', function () {
+    // do something…
+    $('#weight').val("");
+    $('#height').val("");
+    $('#temp').val("");
+    $('#pulse').val("");
+    $('#blood').val("");
+});
+
+ 	console.log('doctor');
+});
