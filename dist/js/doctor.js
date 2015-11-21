5d556a78ad6df52eb1eef8f05d80f97f3a7b2355
singsing <p_111111@hotmail.com>
2015-11-21 10:01:39 +0700

    doctorAddFixBugV2

--- dist/js/doctor.js
+++ dist/js/doctor.js
@@ -17,5 +17,13 @@ $(document).ready(function(){
 		$('#medicineList').html($('#medicineList').html()+html);
     console.log('removed');
     });
+    $('#myModal').on('hidden.bs.modal', function () {
+    // do something…
+    $('#disease_id').val("");
+  $("#medicineList").empty();
+		var html = '<div class="form-group" id="medicineList" ><div class="form-group"><div class="midicine well"> <div class="row"> <div class="col-md-3"> <input placeholder="name" class="name form-control"/> </div><div class="col-md-9"> <input placeholder="usage" class="usage form-control"/> </div></div><div class="row"> <div class="col-md-3"> <input placeholder="quatity" class="quatity form-control"/> </div><div class="col-md-3"> <button class="add btn btn-info">add</button> </div></div></div></div></div>';
+		$('#medicineList').html($('#medicineList').html()+html);
+});
+
  	console.log('doctor');
 });
